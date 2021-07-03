dec4fd02893df231efee4d2ac65b6c85dd923ede
Hafiz ZeeShan <hafizzeeshan138@gmail.com>
2021-07-03 17:27:52 +0500

    fixes Stripe Issue

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -6,12 +6,29 @@ import { selectItems, selectTotal } from "../slices/basketSlice";
 import Currency from "react-currency-formatter";
 import CheckoutProduct from "../components/checkoutProduct";
 import { useSession } from "next-auth/client";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
 
+const stripePromise = loadStripe(process.env.stripe_public_key);
 const Checkout = () => {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
   const [session] = useSession();
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+    // Call the backend to create checkout session
 
+    const checkoutSession = await axios.post("/api/create-checkout-session", {
+      items: items,
+      email: session?.user?.email,
+    });
+
+    // Redirect user/Customer to Stripe checkout
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    });
+    if (result.error) alert(result.error.message);
+  };
   return (
     <div className="bg-gray-100">
       <Header />
@@ -59,6 +76,8 @@ const Checkout = () => {
                 </span>
               </h2>
               <button
+                role="link"
+                onClick={createCheckoutSession}
                 disabled={!session}
                 className={`button mt-2 ${
                   !session &&
